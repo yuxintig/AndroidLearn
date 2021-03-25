a85c692e5f7ca424a4d8198b5268c72eec279e8b
Shinonon <1289037748@qq.com>
2021-03-25 11:59:27 +0800

    完成了Win10计算器页面布局和用Aviators表达式求值。

--- app/src/main/java/com/shinonon/androidlearn/CauActivity.java
+++ app/src/main/java/com/shinonon/androidlearn/CauActivity.java
@@ -1,14 +1,44 @@
 package com.shinonon.androidlearn;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.googlecode.aviator.AviatorEvaluator;
+
 public class CauActivity extends AppCompatActivity {
 
+    EditText input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gridlayout);
+        input = findViewById(R.id.tv_input);
+    }
+
+    public void cal(View view) {
+        Object result = AviatorEvaluator.execute(input.getText().toString().replace("x","*"));
+        input.setText(result.toString());
+
+    }
+
+    public void input(View view) {
+        Button btn = (Button) view;
+        input.setText(input.getText().toString().equals("0")?btn.getText().toString():input.getText().toString()+btn.getText());
+    }
+
+    public void clear(View view) {
+        input.setText("0");
+    }
+
+    public void back(View view) {
+
+    }
+
+    public void percent(View view) {
+
     }
 }
