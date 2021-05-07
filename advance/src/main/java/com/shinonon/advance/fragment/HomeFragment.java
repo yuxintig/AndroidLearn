7eb38983ae05c50489d88d3f88462f010b786a69
Shinonon <1289037748@qq.com>
2021-05-07 19:42:50 +0800

    fragment页面

---
+++ advance/src/main/java/com/shinonon/advance/fragment/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.shinonon.advance.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kirshi.framework.base.BaseFragment;
+import com.shinonon.advance.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends BaseFragment<FragmentHomeBinding> {
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public void inCreateView() {
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        v.tvHome.setText("hhhh");
+
+        //v.tvHome.setText(R.string.do_not_stop_believing);
+    }
+
+}
---
+++ advance/src/main/java/com/shinonon/advance/fragment/NotificationFragment.java
@@ -0,0 +1,32 @@
+package com.shinonon.advance.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kirshi.framework.base.BaseFragment;
+import com.shinonon.advance.R;
+import com.shinonon.advance.databinding.FragmentNotificationBinding;
+
+
+public class NotificationFragment extends BaseFragment<FragmentNotificationBinding> {
+
+
+    public static NotificationFragment newInstance() {
+        return new NotificationFragment();
+    }
+
+    @Override
+    public void inCreateView() {
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        v.tvNotification.setText(R.string.life_is_a_very_funny_proposition_after_all);
+    }
+
+}
